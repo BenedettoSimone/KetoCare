af8419fad55dfe84e76c72448567dd8dc00ecbca
Benedetto Simone <sim.benede@gmail.com>
2023-02-24 17:00:27 +0100

    create get measurement method and show data dinamically

--- KetoCare/assets/js/script.js
+++ KetoCare/assets/js/script.js
@@ -1,4 +1,3 @@
-
 /* ================== PATIENT LIST SECTION ================== */
 
 let cardsData = [];
@@ -34,7 +33,7 @@ fetch('http://localhost:5000/patients', {
  * to show name, surname and fiscal code.
  * @param cardsData: array of items;
  */
-function create_cards(){
+function create_cards() {
     const cardsContainer = document.getElementById("cards-container");
 
     // create the patient list
@@ -54,7 +53,7 @@ function create_cards(){
         rightCard.classList.add("right-card");
 
         const name = document.createElement("h2");
-        name.innerText = cardData.name +" "+ cardData.surname;
+        name.innerText = cardData.name + " " + cardData.surname;
         rightCard.appendChild(name);
 
         const id = document.createElement("h5");
@@ -70,6 +69,7 @@ function create_cards(){
         if (index === 0) {
             card.classList.add("active");
             displayPatientInfo(card.id);
+            get_measurements(card.id)
         }
     });
 
@@ -87,6 +87,9 @@ function create_cards(){
 
             // display info in Patient info card
             displayPatientInfo(card.id);
+
+            //set average and measurements data
+            get_measurements(card.id)
         });
     });
 }
@@ -141,6 +144,118 @@ searchInput.addEventListener("input", () => {
 /* ================== END PATIENT LIST SECTION ================== */
 
 
+/* ================== MEASUREMENTS SECTION ================== */
+
+/**
+ * Function to fetch measurements data about a patient. The function get measurement of a date and previous date.
+ * @param fiscal_code: patient fiscal code;
+ */
+
+function get_measurements(fiscal_code) {
+
+    // compute current date and previous date
+    const dates = getCurrentAndPreviousDate();
+
+    // create an array to store the fetched data
+    const measurements = [];
+
+    // fetch data for previous date
+    fetchData(fiscal_code, dates.previousDate, (data) => {
+        measurements.push(data[0]);
+
+        // fetch data for current date
+        fetchData(fiscal_code, dates.previousDate, (data) => {
+            measurements.push(data[0]);
+            console.log(measurements);
+
+            //LEFT CARD - Previous
+            document.querySelector('.cell-1 .card-title span').innerHTML = '| ' + measurements[0].timestamp;
+            document.querySelector('.cell-1 .ph_average').innerHTML = measurements[0].average_value;
+
+            //RIGHT CARD - Current
+            document.querySelector('.cell-2 .card-title span').innerHTML = '| ' + measurements[1].timestamp;
+            document.querySelector('.cell-2 .ph_average').innerHTML = measurements[1].average_value;
+
+            updateChart(dates.previousDate, measurements[0].values, dates.currentDate, measurements[1].values);
+
+        });
+    });
+
+}
+
+/**
+ * Function to fetch measurement data with fiscal code and date.
+ * @param fiscal_code: patient fiscal code;
+ * @param date: timestamp;
+ * @param callback:
+ */
+function fetchData(fiscal_code, date, callback) {
+    const input = {
+        fiscal_code: fiscal_code,
+        date: date
+    };
+
+    fetch('http://localhost:5000/measurements', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(input)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: status code ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            callback(data);
+        })
+        .catch(error => {
+            console.log(`Error: ${error.message}`);
+        });
+}
+
+/**
+ * Function to compute current date and previous date.
+ */
+function getCurrentAndPreviousDate() {
+    const today = new Date();
+    const previousDate = new Date(today);
+    previousDate.setDate(today.getDate() - 1);
+    const todayFormatted = today.toISOString().split('T')[0];
+    const previousDateFormatted = previousDate.toISOString().split('T')[0];
+
+    return {currentDate: todayFormatted, previousDate: previousDateFormatted};
+}
+
+/**
+ * Function to update series in line chart.
+ * @param previousDate: previous date;
+ * @param previousData: series data of previous date;
+ * @param currentDate: current date;
+ * @param currentData: series data of current date;
+ */
+function updateChart(previousDate, previousData, currentDate, currentData){
+
+    console.log(typeof previousDate)
+    console.log(typeof previousData)
+
+    chart.updateSeries([{
+      name: previousDate,
+      data: JSON.parse(previousData)
+    }, {
+      name: currentDate,
+      data: JSON.parse(currentData)
+    }]);
+
+}
+
+
+/* ================== END MEASUREMENTS SECTION ================== */
+
+
 /* ================== PATIENT INFO SECTION ================== */
 
 
@@ -189,7 +304,7 @@ function displayPatientInfo(fiscalCode) {
     const diabetTypeCard = patientInfoDiv.querySelector("#diabet_type");
 
     img.src = "./assets/img/patient_1.jpg";
-    h2.textContent = patient.name +" "+patient.surname;
+    h2.textContent = patient.name + " " + patient.surname;
     h4.textContent = patient.fiscal_code;
 
     birthdayCard.querySelector("span").textContent = computeAge(patient.birthdate) + " y/old";
--- app.py
+++ app.py
@@ -11,13 +11,41 @@ dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:4566')
 @app.route('/patients', methods=['GET'])
 @cross_origin()
 def get_all_patients():
-
     patients_table = dynamodb.Table('Patients')
     response = patients_table.scan()
     items = response['Items']
+    print(items)
 
     res = make_response(jsonify(items), 200)
     return res
+
+
+@app.route('/measurements', methods=['POST'])
+@cross_origin()
+def get_measurements():
+
+    data = request.get_json()
+    fiscal_code = data.get('fiscal_code')
+    date = data.get('date')
+
+    measurements_table = dynamodb.Table("Averages")
+
+    response = measurements_table.query(
+        KeyConditionExpression='fiscal_code = :fc AND begins_with(#timestamp, :ts)',
+        ExpressionAttributeValues={
+            ':fc': fiscal_code,
+            ':ts': date
+        },
+        ExpressionAttributeNames={
+            '#timestamp': 'timestamp'
+        }
+    )
+
+    items = response['Items']
+    print(items)
+    res = make_response(jsonify(items), 200)
+    return res
+
 
 if __name__ == '__main__':
     app.run(debug=True)
