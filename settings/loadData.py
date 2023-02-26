761deee6edbd5ea658570daebb8f4aefe5ceb0d0
Benedetto Simone <sim.benede@gmail.com>
2023-02-26 19:06:52 +0100

    load data for 2 past dates

--- settings/loadData.py
+++ settings/loadData.py
@@ -16,47 +16,48 @@ devices = [('01', 'FRRLNZ50M24F839C'),
            ('04', 'RMNLRT80T03A783U'),
            ('05', 'BRBSMN63P26F205X')]
 
-for device in devices:
-    values = []
-    for i in range(0, 7):
-        # Compute yesterday
-        measure_date = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S"))
+for day in range(1, 3):
+    for device in devices:
+        values = []
+        for i in range(0, 7):
+            # Compute yesterday
+            measure_date = str((datetime.datetime.now() - datetime.timedelta(days=day)).strftime("%Y-%m-%d %H:%M:%S"))
 
-        if random.random() > 0.10:
-            # Generate regular pH value
-            blood_pH = round(random.uniform(7.35, 7.45), 2)
-            values.append(blood_pH)
-        else:
-            # Generate warning pH value
-            blood_pH = round(random.uniform(4.0, 7.34), 2)
-            values.append(blood_pH)
+            if random.random() > 0.10:
+                # Generate regular pH value
+                blood_pH = round(random.uniform(7.35, 7.45), 2)
+                values.append(blood_pH)
+            else:
+                # Generate warning pH value
+                blood_pH = round(random.uniform(4.0, 7.34), 2)
+                values.append(blood_pH)
+
+            item = {
+                'fiscal_code': device[1],
+                'timestamp': measure_date,
+                'device_id': device[0],
+                'measured_value': str(blood_pH)
+            }
+
+            measurements_table.put_item(Item=item)
+
+            print("Stored item", item)
+
+            # It is necessary to obtain the measurements at different times, otherwise the data will be overwritten.
+            time.sleep(1)
+
+        # Compute yesterday
+        measure_date = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+
+        mean_pH = round(statistics.mean(values), 2)
 
         item = {
             'fiscal_code': device[1],
             'timestamp': measure_date,
             'device_id': device[0],
-            'measured_value': str(blood_pH)
+            'average_value': str(mean_pH),
+            'values': str(values)
         }
-
-        measurements_table.put_item(Item=item)
+        averages_table.put_item(Item=item)
 
         print("Stored item", item)
-
-        # It is necessary to obtain the measurements at different times, otherwise the data will be overwritten.
-        time.sleep(1)
-
-    # Compute yesterday
-    measure_date = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
-
-    mean_pH = round(statistics.mean(values), 2)
-
-    item = {
-        'fiscal_code': device[1],
-        'timestamp': measure_date,
-        'device_id': device[0],
-        'average_value': str(mean_pH),
-        'values': str(values)
-    }
-    averages_table.put_item(Item=item)
-
-    print("Stored item", item)
